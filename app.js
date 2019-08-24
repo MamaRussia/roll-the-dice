41c6949a1592a5bda11547fb058af318f89c8b05
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-08-24 15:36:37 -0700

    dice now update current score for player unless rolls 1

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores , roundScore, activePlayer;
+
+score = [0, 0];
+roundScore = 0; 
+activePlayer = 1;
+
+let x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM = document.querySelector(".dice")
+    diceDOM.style.display = "block";
+    diceDOM.src = "./images/dice-" + dice + ".png";
+    if (dice !== 1) {
+        document.querySelector("#current-" + activePlayer).textContent = dice;
+    }
+
+})
